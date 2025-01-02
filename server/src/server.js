4fb67d262b9e67a79c3787bf02b83278fe2c8521
lupin4 <davidclabaugh@fullsail.edu>
2025-01-02 10:40:26 -0800

    added mvc code requests, model and controller

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const app = require('./app');
-
+const [{ loadPlanetsData }] = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+await loadPlanetsData();
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
